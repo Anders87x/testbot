fda9cce14929ee015676a0c7cac0e9f9c1f11c05
AnderCode <davisanderson87@gmail.com>
2023-10-09 20:36:02 -0500

    09/10/23 Subiendo a github 4

--- index.php
+++ index.php
@@ -25,11 +25,28 @@ if (isset($update->message->text)) {
     } elseif ($text === '/menu') {
         // Responde con el menú de opciones
         $menuMessage = "Aquí está el menú de opciones:\n";
-        $menuMessage .= "1. Opción 1\n";
-        $menuMessage .= "2. Opción 2\n";
-        $menuMessage .= "3. Opción 3\n";
+        $menuMessage .= "1. Informacion del Curso 1\n";
+        $menuMessage .= "2. Ubicacion del local 2\n";
+        $menuMessage .= "3. Enviar Temario en PDF 3\n";
+        $menuMessage .= "4. Enviar Audio Explicando el curso 4\n";
+        $menuMessage .= "5. Video de Introduccion 5\n";
+        $menuMessage .= "6. Hablar con AnderCode 6\n";
+        $menuMessage .= "7. Horario de Atencion 7\n";
 
         $telegram->sendMessage($chatId, $menuMessage);
+    } elseif ($text === '1') {
+        // Responde con el menú de opciones
+        $menuMessage = "Aquí está el menú de opciones:\n";
+
+        $telegram->sendMessage($chatId, $menuMessage);
+
+    } elseif ($text === '2') {
+        // Define las coordenadas de latitud y longitud
+        $latitude = 51.5074; // Cambia esto a la latitud deseada
+        $longitude = -0.1278; // Cambia esto a la longitud deseada
+
+        // Envia la ubicación
+        $telegram->sendLocation($chatId, $latitude, $longitude);
     } else {
         // Si el mensaje no coincide con ningún comando, responde con un mensaje predeterminado
         $defaultMessage = "No entiendo ese comando. Puedes usar /start para iniciar o /menu para ver el menú.";
