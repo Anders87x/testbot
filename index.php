5a3968967920569d6a569d331188bb3639cd7bac
AnderCode <davisanderson87@gmail.com>
2023-10-09 22:04:24 -0500

    26/09/23 Subiendo a github 19

--- index.php
+++ index.php
@@ -36,11 +36,11 @@ if (isset($update->message->text)) {
 
         $telegram->sendMessage($chatId, $menuMessage);
     } elseif ($text === '/url') {
-       /*  $url = 'https://anderson-bastidas.com';
+        $url = 'https://anderson-bastidas.com';
         $img = 'assets/img.png';
 
         // Envía el enlace al video de YouTube
-        $telegram->sendPhoto($chatId, $img, $url); */
+        $telegram->sendPhoto($chatId, $img, $url);
     } elseif ($text === '1') {
         // Responde con el menú de opciones
         $menuMessage = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
